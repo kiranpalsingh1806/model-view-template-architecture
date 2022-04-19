24482eaf0eecee2fdc1570cffe49f6f20ae3a3c5
Kiranpal Singh <kiranpalsingh1806@gmail.com>
2022-04-19 18:44:06 +0530

    Add Database Setup Connection and Store in DB

--- Hello/Hello/settings.py
+++ Hello/Hello/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'home.apps.HomeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +129,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
     '/var/www/static/',
 ]
+
+MESSAGE_TAGS = {
+    messages.INFO: '',
+    50: 'critical',
+}
--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render
+from datetime import datetime
+from home.models import Contact
 
 # Create your views here.
 
@@ -24,5 +26,14 @@ def services(request):
 
 
 def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        desc = request.POST.get('desc')
+
+        contact = Contact(name=name, email=email, phone=phone,
+                          desc=desc, date=datetime.today())
+        contact.save()
+
     return render(request, 'contact.html')
-    # return HttpResponse("This is Contact Page")
